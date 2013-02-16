d06effa8d46d7388b84a0d6d81df8ec99da8b1db
Ethan Vizitei <ethan.vizitei@gmail.com>
2013-02-15 22:23:57 -0600

    add additional test cases
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

--- spec/lib/tf1_converter/translation_spec.rb
+++ spec/lib/tf1_converter/translation_spec.rb
@@ -3,11 +3,19 @@ require_relative '../../../lib/tf1_converter'
 
 module TF1Converter
   describe Translation do
-    it "translates a file correctly" do
+    before do
       execution_time = Time.local(2008, 9, 1, 12, 0, 0)
       Timecop.freeze(execution_time)
-      local_dir = File.dirname(__FILE__)
-      path = File.expand_path('../../fixtures', local_dir)
+    end
+
+    after do
+      Timecop.return
+    end
+
+    let(:local_dir) { File.dirname(__FILE__) }
+    let(:path) { File.expand_path('../../fixtures', local_dir) }
+
+    it "translates a file correctly" do
       input = File.open("#{path}/test.gpx", 'r')
       output = File.open("#{path}/test.kml", 'w')
       expected = File.open("#{path}/expected.kml", 'r')
@@ -16,7 +24,14 @@ module TF1Converter
 
       result = File.open("#{path}/test.kml", 'r')
       result.read.should == expected.read
-      Timecop.return
+    end
+
+    it 'translates a tracks-only file' do
+      input = File.open("#{path}/ftwood2.gpx", 'r')
+      output = File.open("#{path}/ftwood2.kml", 'w')
+      TF1Converter::Config.load(File.expand_path('../../../example/config.yml', local_dir))
+      #should not raise error
+      TF1Converter::Translation.from(input).into(output)
     end
   end
 end
