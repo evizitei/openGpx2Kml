243d640be68abe3803da300d58a9df08ff7cf36b
Ethan Vizitei <ethan.vizitei@gmail.com>
2013-03-27 09:19:41 -0500

    left pad track colors with 0s

--- lib/tf1_converter/kml/track_color.rb
+++ lib/tf1_converter/kml/track_color.rb
@@ -1,18 +1,19 @@
 module TF1Converter::Kml
   class TrackColor
     def self.next
-      if config.use_constant_color
+      return_color = if config.use_constant_color
         config.constant_color
       else
         @colors ||= config.colors.values
         @color_index ||= 0
-        return_color = @colors[@color_index]
+        rc = @colors[@color_index]
         @color_index += 1
         if @color_index >= @colors.length
           @color_index = 0
         end
-        return_color
+        rc
       end
+      return_color.rjust(8, '0')
     end
 
     def self.uncache!
--- spec/lib/tf1_converter/kml/track_color_spec.rb
+++ spec/lib/tf1_converter/kml/track_color_spec.rb
@@ -3,6 +3,8 @@ require_relative '../../../../lib/tf1_converter/kml/track_color'
 
 module TF1Converter::Kml
   describe TrackColor do
+    let(:colors) { [nil, nil].map{|n| TrackColor.next } }
+
     before(:each) do
       ::TF1Converter::Config.stub(:colors){ {'Blue'=>'1', 'Red'=>'2', 'Yellow'=>'3'}}
     end
@@ -12,18 +14,33 @@ module TF1Converter::Kml
       ::TF1Converter::Config.unstub!(:colors)
     end
 
-    it 'cycles colors when configured that way' do
-      ::TF1Converter::Config.stub(:use_constant_color){ false }
-      colors = [nil, nil].map{|n| TrackColor.next }
-      colors[0].should_not == colors[1]
+    describe 'no constant coloring' do
+      before { ::TF1Converter::Config.stub(:use_constant_color){ false } }
+
+      it 'cycles colors when configured that way' do
+        colors[0].should_not == colors[1]
+      end
+
+      it 'left pads each color' do
+        colors.each{ |v| v.should =~ /^0000000\d$/ }
+      end
     end
 
-    it 'generates a constant color when constant color switch flipped in config' do
-      ::TF1Converter::Config.stub(:use_constant_color){ true }
-      ::TF1Converter::Config.stub(:constant_color){ 'c12345' }
-      colors = [nil, nil].map{|n| TrackColor.next }
-      colors[0].should == colors[1]
-      ::TF1Converter::Config.unstub!(:constant_color)
+    describe 'constant coloring' do
+
+      before do
+        ::TF1Converter::Config.stub(:use_constant_color){ true }
+        ::TF1Converter::Config.stub(:constant_color){ 'c12345' }
+      end
+
+      it 'generates a constant color when constant color switch flipped in config' do
+        colors[0].should == colors[1]
+      end
+
+      it 'left pads the color' do
+        colors[0].should == '00c12345'
+      end
     end
+
   end
 end
