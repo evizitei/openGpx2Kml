3f760ba40563284ffbfa8a4bce9c64f707d19f3b
Ethan Vizitei <ethan.vizitei@gmail.com>
2013-03-28 14:43:24 -0500

    fix csv output for filenames

--- lib/tf1_converter/csv_file.rb
+++ lib/tf1_converter/csv_file.rb
@@ -11,13 +11,21 @@ module TF1Converter
       CSV.open(@path, 'wb') do |csv|
         csv << ['filename', 'name', 'meaning', 'time', 'lat', 'long', 'usng', 'elevation']
         @waypoints.each do |wp|
-          csv << [@path.split('.').first, wp.name, wp.icon_meaning, wp.timestamp, wp.lat, wp.long, wp.usng, wp.elevation]
+          csv << [raw_path, wp.name, wp.icon_meaning, wp.timestamp, wp.lat, wp.long, wp.usng, wp.elevation]
         end
       end
     end
 
+    def raw_path
+      CsvFile.raw_path(@path)
+    end
+
     def self.translate_filename(file_path)
-      file_path.split('.')[0...-1].join('.') + '.csv'
+      raw_path(file_path) + '.csv'
+    end
+
+    def self.raw_path(path)
+      path.split('.')[0...-1].join('.')
     end
   end
 end
--- spec/lib/tf1_converter/csv_file_spec.rb
+++ spec/lib/tf1_converter/csv_file_spec.rb
@@ -2,6 +2,16 @@ require_relative '../../../lib/tf1_converter/csv_file'
 
 module TF1Converter
   describe CsvFile do
+
+    it 'writes waypoints to the csv file' do
+      waypoints = [double('waypoint').as_null_object]
+      base_path = '/some/long.path/with.file/test.gpx'
+      csv = []
+      CSV.stub(:open).and_yield(csv)
+      CsvFile.new(waypoints, base_path).to_csv!
+      csv[1].first.should == '/some/long.path/with.file/test'
+    end
+
     describe '.translate_filename' do
       it 'changes a filepath to be a csv filename' do
         base_path = '/some/long/path/with/file'
